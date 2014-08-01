d730bbdd9f0a47435f91fe57aa57e28290c7ff9e
James Martin <james@roomz.com>
2014-08-01 15:27:16 +1000

    Basic letter template

--- app.rb
+++ app.rb
@@ -3,7 +3,12 @@ require 'sinatra/asset_pipeline'
 class WebApp < Sinatra::Base
   register Sinatra::AssetPipeline
 
-  get '/' do
-    haml :index
+  get '/letter/:name' do
+    @name = params[:name]
+    begin
+      haml "letter_to_#{@name.downcase}".to_sym
+    rescue Errno::ENOENT
+      haml :letter
+    end
   end
 end
